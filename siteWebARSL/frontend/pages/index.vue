5416c598da8440d003d67f0a115897241213f526
FredericCanaud <frederic.canaud@etu.unilim.fr>
2019-11-21 07:55:12 +0100

    siteWebdébut

---
+++ siteWebARSL/frontend/pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+      <img src="/logo.png" height="200" width="200" class="uk-align-center" alt="">
+      <h1> Vous rencontrez des difficultés sociales (absence ou perte de logement, violences conjugales, expulsions...) ? </h1>
+      <h1> Appelez le 115 ! </h1>
+      <body>
+          <h2> L'ARSL et les associations sur Limoges </h2>
+          <p> ’Association de réinsertion sociale du Limousin est une association loi 1901 créée en 1961.
+          Son objet est de lutter contre toutes les formes d’exclusion sociale.
+          Elle accueille des personnes en difficulté, et les accompagne dans leur démarche d’intégration
+          sociale et de citoyenneté. L’ARSL est financée par l’Etat et par le Conseil départemental 87,
+          la ville de Limoges, la Caisse d’allocations familiales (CAF), et d’autres organismes ;
+          Elle emploie aujourd’hui une centaine de salariés. </p>
+      </body>
+
+  </div>
+
+</template>
