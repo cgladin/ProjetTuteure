0b4ec8964c4487031469b65a4c7950a1f98c2af4
ThomasCampredon <thomas.campredon@etu.unilim.fr>
2020-01-13 10:35:58 +0100

    en fait on s'en fou du frontend
    sadness

--- CODE/siteWebARSL/frontend/pages/lieux.vue
+++ CODE/siteWebARSL/frontend/pages/lieux.vue
@@ -6,50 +6,50 @@
 
         <div class=".align-content-center">
             <form class="vertical">
-                <div class="span6">
+                <div class="span5">
                 <fieldset>
-                <legend>Information du lieu</legend>
-                <div class="control-group">
-                    <label class="control-label" for="input01">Association responsable :</label>
-                    <div class="controls">
-                        <div class="form-group">
-                            <select class="form-control" id="sel1">
-                                <option>Croix rouge</option>
-                                <option>recuperer avec strapi</option>
-                                <option>aled</option>
-                                <option>.....</option>
-                            </select>
-                         </div> 
-                    <p class="help-block">Selectionner l'association qui est responsable du lieu d'accueil</p>
-                    </div>
-                </div>
+                    <legend>Information du lieu</legend>
                     <div class="control-group">
-                    <label class="control-label" for="input01">Nom du lieu :</label>
-                    <div class="controls">
-                        <input type="text" class="input-xlarge" id="input01">
-                        <p class="help-block">Quel nom est communément donné à ce lieu (ex : gymnase Jean Martin)</p>
-                    </div>
+                        <label class="control-label" for="input01">Association responsable :</label>
+                        <div class="controls">
+                            <div class="form-group">
+                                <select class="form-control" id="sel1" style="width:auto;">
+                                    <option>Croix rouge</option>
+                                    <option>recuperer avec strapi</option>
+                                    <option>aled</option>
+                                    <option>.....</option>
+                                </select>
+                            </div> 
+                        <p class="help-block">Selectionner l'association qui est responsable du lieu d'accueil</p>
+                        </div>
                     </div>
                     <div class="control-group">
-                    <label class="control-label" for="input01">Heure d'ouverture :</label>
-                    <div class="controls">
-                        <input type="time" name="time" />
-                        <p class="help-block">A quelle heure le lieu ouvre au publique</p>
+                        <label class="control-label" for="input01">Nom du lieu :</label>
+                        <div class="controls">
+                            <input type="text" class="input-xlarge" id="input01">
+                            <p class="help-block">Quel nom est communément donné à ce lieu (ex : gymnase Jean Martin)</p>
+                        </div>
                     </div>
+                    <div class="control-group">
+                        <label class="control-label" for="input01">Heure d'ouverture :</label>
+                        <div class="controls">
+                            <input type="time" name="time" class="timehour">
+                            <p class="help-block">A quelle heure le lieu ouvre au publique</p>
+                        </div>
                     </div> 
                     <div class="control-group">
-                    <label class="control-label" for="input01">Heure de fermeture :</label>
-                    <div class="controls">
-                        <input type="time" name="time" />
-                        <p class="help-block">A quelle heure le lieu ferme au publique</p>
-                    </div>
+                        <label class="control-label" for="input01">Heure de fermeture :</label>
+                        <div class="controls">
+                            <input type="time" name="time" class="timehour">
+                            <p class="help-block">A quelle heure le lieu ferme au publique</p>
+                        </div>
                     </div>   
                     <div class="control-group">
-                    <label class="control-label" for="input01">Adresse postale :</label>
-                    <div class="controls">
-                        <input type="text" class="input-xlarge" id="input01">
-                        <p class="help-block">ex : 27 rue du général de Gaulle, 87000 Limoges France</p>
-                    </div>
+                        <label class="control-label" for="input01">Adresse postale :</label>
+                        <div class="controls">
+                            <textarea class="form-control" rows="2" id="comment" style="width:auto;"></textarea>
+                            <p class="help-block">ex : 27 rue du général de Gaulle, 87000 Limoges France</p>
+                        </div>
                     </div>           
                 </fieldset>
                 </div>
@@ -59,12 +59,29 @@
     </div>
 </template>
 
+
+
+
+
 <style>
     .help-block{
         font-size:12px;
     }
+    .input-xlarge{
+        border-radius:3px;
+    }
+    .timehour{
+        border-radius:3px;
+    }
+    .form-control{
+        border-radius:3px;
+    }
+    .control-group{
+        margin-bottom:25px;
+    }
     #titre{
-        margin-top:4%;
+        margin-top:5%;
+
     }
 </style>
 
