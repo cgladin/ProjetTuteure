4cfad1b45df6246adc53b3d3ffb91d942a6a54ae
lordgravier <baskette11@gmail.com>
2020-01-15 11:28:09 +0100

    trad seulement sur un ecran pc

--- CODE/siteWebARSL/frontend/components/Header.vue
+++ CODE/siteWebARSL/frontend/components/Header.vue
@@ -7,21 +7,19 @@
 
                    <!-- burger sur telephone -->
                    <div class="row tel">
-
                        <b-button v-b-toggle.collapse-1 variant="primary" class="tel bg-dark">
                          <span class="navbar-toggler-icon "></span>
                        </b-button>
 
-
                        <b-collapse id="collapse-1" class="mt-2">
                          <b-card class="bg-dark">
-                           <div class="translate" id="google_translate_element"></div>
                            <router-link tag="a" class="nav-link text-white" to="/associations" exact>Associations</router-link>
                            <a class="nav-link text-white" href="http://localhost:1337/admin/auth/login">Se connecter</a>
                          </b-card>
                        </b-collapse>
                  </div>
 
+                  <!-- bouton sur pc -->
                      <ul class="navbar-nav ml-auto ordi">
                        <li class="nav-item">
                          <div class="translate" id="google_translate_element"></div>
@@ -38,7 +36,7 @@
                  <!-- javascript du translate -->
                  <script type="text/javascript">// <![CDATA[
                  function googleTranslateElementInit() {
-                 new google.translate.TranslateElement({pageLanguage: 'fr', layout: google.translate.TranslateElement.InlineLayout.SIMPLE}, 'google_translate_element');
+                 new google.translate.TranslateElement({pageLanguage: 'fr', includedLanguages:'en,ar,ru', layout: google.translate.TranslateElement.InlineLayout.SIMPLE}, 'google_translate_element');
                  }
                  // ]]></script>
                  <script src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit" type="text/javascript"></script>
