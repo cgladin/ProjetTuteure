6c05f9457481976c10083c3d229331f14410d234
Frédéric CANAUD <frederic.canaud@etu.unilim.fr>
2020-01-15 10:23:54 +0100

    Finish Formulaire Asso + ajout attributs Formulaire

--- CODE/siteWebARSL/frontend/pages/formulaireMaraude/index.vue
+++ CODE/siteWebARSL/frontend/pages/formulaireMaraude/index.vue
@@ -1,9 +1,9 @@
 
 <template>
   <div>
-  <b-card no-body>
-
-        <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+  <b-card>
+        <h1 class="text-align-center mt-3 text-center"> Formulaire de maraude pour personne rencontrée </h1>
+        <b-form id="formulaireMaraude" @submit="onSubmit" @reset="onReset" v-if="show">
           <b-tabs card fill>
             <b-tab title="Identité" active>
       <b-form-group
@@ -43,33 +43,69 @@
         placeholder="Nationalité..."
         ></b-form-input>
       </b-form-group>
-      <b-form-group id="textarea-group-1" label="Information Famille :" label-for="textarea-1">
+      <b-form-group id="input-group-5" label="Information Famille :" label-for="input-5">
         <b-form-textarea
-              id="textarea-1"
+              id="input-5"
               v-model="form.infoFamille"
               placeholder="Information famille..."
               rows="4"
               max-rows="8"
         ></b-form-textarea>
       </b-form-group>
-      <b-form-group id="input-group-5" label="Lieu de rencontre :" label-for="input-5">
+      <b-form-group id="input-group-6" label="Lieu de rencontre :" label-for="input-6">
         <b-form-input
-        id="input-5"
+        id="input-6"
         v-model="form.lieu"
         placeholder="Lieu de rencontre..."
         ></b-form-input>
       </b-form-group>
       </b-tab>
       <b-tab title="Santé">
-        <b-card-text>Tab contents 2</b-card-text>
+        <b-form-group id="input-group-7" label="Etat de grossesse :" label-for="input-7">
+          <b-form-radio id="input-7" v-model="selected" name="positif" value="true">Positif</b-form-radio>
+          <b-form-radio id="input-7" v-model="selected" name="negatif" value="false">Négatif</b-form-radio>
+        </b-form-group>
+        <b-form-group id="input-group-8" label="Problèmes de santé :" label-for="input-8">
+          <b-form-textarea
+                id="input-8"
+                v-model="form.problemeDeSante"
+                placeholder="Problème de santé..."
+                rows="4"
+                max-rows="8"
+          ></b-form-textarea>
+        </b-form-group>
+        <b-form-group id="input-group-9" label="Autres problèmes :" label-for="input-9">
+          <b-form-textarea
+                id="input-9"
+                v-model="form.autre"
+                placeholder="Autres problèmes..."
+                rows="4"
+                max-rows="8"
+          ></b-form-textarea>
+        </b-form-group>
       </b-tab>
       <b-tab title="Hébergement">
-        <b-card-text>Tab contents 2</b-card-text>
+        <b-form-group id="input-group-10" v-model="form.hebergementActuel" label="Situation actuelle d'hébergement de la personne :" label-for="input-10">
+          <b-form-radio v-model="selected" name="true" value="A">Positif</b-form-radio>
+          <b-form-radio v-model="selected" name="false" value="B">Négatif</b-form-radio>
+        </b-form-group>
+        <b-form-group id="input-group-11" v-model="form.dejaConnu" label="La personne est-elle déjà connu de la maraude ?" label-for="input-11">
+          <b-form-radio v-model="selected" name="oui" value="true">Oui</b-form-radio>
+          <b-form-radio v-model="selected" name="non" value="false">Non</b-form-radio>
+        </b-form-group>
+        <b-form-group id="input-group-12" v-model="form.demandeHebergement" label="La personne a déjà exprimé une demande d'hébergement ?" label-for="input-12">
+          <b-form-radio v-model="selected" name="oui" value="true">Oui</b-form-radio>
+          <b-form-radio v-model="selected" name="non" value="false">Non</b-form-radio>
+        </b-form-group>
+        <b-form-group id="input-group-13" v-model="form.demandeAcceptee" label="Le 115 a t-il répondu positivement à la demande ?" label-for="input-13">
+          <b-form-radio v-model="selected" name="oui" value="true">Oui</b-form-radio>
+          <b-form-radio v-model="selected" name="non" value="false">Non</b-form-radio>
+        </b-form-group>
       </b-tab>
       </b-tabs>
-      <b-button type="submit" variant="primary">Submit</b-button>
-      <b-button type="reset" variant="danger">Reset</b-button>
-
+      <div id="valider" class="d-flex justify-content-center ">
+        <b-button type="submit" variant="primary">Valider le formulaire</b-button>
+      </div>
     </b-form>
 
 
