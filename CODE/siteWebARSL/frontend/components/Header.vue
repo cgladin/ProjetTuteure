29689432159cd21cc6c42e8bc05741141141cc73
Frédéric CANAUD <frederic.canaud@etu.unilim.fr>
2020-01-16 08:35:23 +0100

    Identation correcte des pages vue

--- CODE/siteWebARSL/frontend/components/Header.vue
+++ CODE/siteWebARSL/frontend/components/Header.vue
@@ -1,46 +1,51 @@
 
 <template>
-  <client-only>
-               <nav id="navigation" class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-                 <div class="container">
-                   <a href="/"><b-img src="logo.png" height="60" width="60" fluid alt="Responsive logo"></b-img></a>
+   <client-only>
 
-                   <!-- burger sur telephone -->
-                   <div class="row tel">
-                       <b-button v-b-toggle.collapse-1 variant="primary" class="tel bg-dark">
-                         <span class="navbar-toggler-icon "></span>
-                       </b-button>
+      <nav id="navigation" class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+         <div class="container">
+            <a href="/">
+               <b-img src="logo.png" height="60" width="60" fluid alt="Responsive logo"></b-img>
+            </a>
 
-                       <b-collapse id="collapse-1" class="mt-2">
-                         <b-card class="bg-dark">
-                           <router-link tag="a" class="nav-link text-white" to="/associations" exact>Associations</router-link>
-                           <a class="nav-link text-white" href="http://localhost:1337/admin/auth/login">Se connecter</a>
-                         </b-card>
-                       </b-collapse>
-                 </div>
+            <!-- burger sur telephone -->
+            <div class="row tel">
+               <b-button v-b-toggle.collapse-1 variant="primary" class="tel bg-dark">
+                  <span class="navbar-toggler-icon "></span>
+               </b-button>
+               <b-collapse id="collapse-1" class="mt-2">
+                  <b-card class="bg-dark">
+                     <router-link tag="a" class="nav-link text-white" to="/associations" exact>Associations</router-link>
+                     <a class="nav-link text-white" href="http://localhost:1337/admin/auth/login">Se connecter</a>
+                  </b-card>
+               </b-collapse>
+            </div>
 
-                  <!-- bouton sur pc -->
-                     <ul class="navbar-nav ml-auto ordi">
-                       <li class="nav-item">
-                         <div class="translate" id="google_translate_element"></div>
-                       </li>
-                       <li class="nav-item align-middle">
-                         <router-link tag="a" class="nav-link text-white" to="/associations" exact>Associations</router-link>
-                       </li>
-                       <li class="nav-item align-middle">
-                         <a class="nav-link text-white" href="http://localhost:1337/admin/auth/login">Se connecter</a>
-                       </li>
-                     </ul>
-                 </div>
+            <!-- bouton sur pc -->
+            <ul class="navbar-nav ml-auto ordi">
+               <li class="nav-item">
+                  <div class="translate" id="google_translate_element"></div>
+               </li>
+               <li class="nav-item align-middle">
+                  <router-link tag="a" class="nav-link text-white" to="/associations" exact>Associations</router-link>
+               </li>
+               <li class="nav-item align-middle">
+                  <a class="nav-link text-white" href="http://localhost:1337/admin/auth/login">Se connecter</a>
+               </li>
+            </ul>
 
-                 <!-- javascript du translate -->
-                 <script type="text/javascript">// <![CDATA[
-                 function googleTranslateElementInit() {
-                 new google.translate.TranslateElement({pageLanguage: 'fr', includedLanguages:'en,ar,ru', layout: google.translate.TranslateElement.InlineLayout.SIMPLE}, 'google_translate_element');
-                 }
-                 // ]]></script>
-                 <script src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit" type="text/javascript"></script>
-               </nav>
+         </div>
 
-  </client-only>
+         <!-- javascript du translate -->
+         <script type="text/javascript">// <![CDATA[
+            function googleTranslateElementInit() {
+            new google.translate.TranslateElement({pageLanguage: 'fr', includedLanguages:'en,ar,ru', layout: google.translate.TranslateElement.InlineLayout.SIMPLE}, 'google_translate_element');
+            }
+            // ]]>
+         </script>
+         <script src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit" type="text/javascript"></script>
+         
+      </nav>
+
+   </client-only>
 </template>
--- CODE/siteWebARSL/frontend/pages/associations/_id.vue
+++ CODE/siteWebARSL/frontend/pages/associations/_id.vue
@@ -1,55 +1,53 @@
 <template>
-  <div>
+   <div>
 
       <form class="uk-search uk-search-large uk-align-center uk-margin">
-          <span uk-search-icon></span>
-          <input class="uk-search-input" v-model="query" type="search" placeholder="Search...">
+         <span uk-search-icon></span>
+         <input class="uk-search-input" v-model="query" type="search" placeholder="Search...">
       </form>
 
       <div class="uk-card uk-card-default uk-grid-collapse uk-child-width-1-2@m uk-margin" v-for="association in filteredList" v-bind:key="association" uk-grid>
-          <div class="uk-card-media-left uk-cover-container">
-              <img :src="'http://localhost:1337/' + association.image.url" alt="" uk-cover>
-              <canvas width="600" height="400"></canvas>
-          </div>
-          <div>
-              <div class="uk-card-body">
-                  <h3 class="uk-card-title">{{ association.nom }}</h3>
-                  <p>{{ association.description }}</p>
-                  <router-link :to="{ name: 'associations-id', params: { id: association.id }}" tag="a" class="uk-button uk-button-primary">Voir l'association
-                  </router-link>
-              </div>
-          </div>
+         <div class="uk-card-media-left uk-cover-container">
+            <img :src="'http://localhost:1337/' + association.image.url" alt="" uk-cover>
+            <canvas width="600" height="400"></canvas>
+         </div>
+         <div>
+            <div class="uk-card-body">
+               <h3 class="uk-card-title">{{ association.nom }}</h3>
+               <p>{{ association.description }}</p>
+               <router-link :to="{ name: 'associations-id', params: { id: association.id }}" tag="a" class="uk-button uk-button-primary">Voir l'association</router-link>
+            </div>
+         </div>
       </div>
-
-  </div>
-
+      
+   </div>
 </template>
 
 <script>
-// Import the associations query
-import associationsQuery from '~/apollo/queries/association/associations'
+   // Import the associations query
+   import associationsQuery from '~/apollo/queries/association/associations'
 
-export default {
-  data() {
-    return {
-      // Initialize an empty restaurants variabkle
-      associations: [],
-      query: ''
-    }
-  },
-  apollo: {
-    association: {
-      prefetch: true,
-      query: associationsQuery
-    }
-  },
-  computed: {
-    // Search system
-    filteredList() {
-      return this.associations.filter(restaurant => {
-        return association.nom.toLowerCase().includes(this.query.toLowerCase())
-      })
-    },
-  }
-}
+   export default {
+     data() {
+       return {
+         // Initialize an empty restaurants variabkle
+         associations: [],
+         query: ''
+       }
+     },
+     apollo: {
+       association: {
+         prefetch: true,
+         query: associationsQuery
+       }
+     },
+     computed: {
+       // Search system
+       filteredList() {
+         return this.associations.filter(restaurant => {
+           return association.nom.toLowerCase().includes(this.query.toLowerCase())
+         })
+       },
+     }
+   }
 </script>
