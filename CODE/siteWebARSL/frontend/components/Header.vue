296854cb664eae014fddc598296d0437db9d61e2
Oydrey <oydrey@gmail.com>
2020-01-16 10:43:52 +0100

    ajout question et picto, à rendre plus joli

--- CODE/siteWebARSL/frontend/components/Header.vue
+++ CODE/siteWebARSL/frontend/components/Header.vue
@@ -44,7 +44,7 @@
             // ]]>
          </script>
          <script src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit" type="text/javascript"></script>
-         
+
       </nav>
 
    </client-only>
