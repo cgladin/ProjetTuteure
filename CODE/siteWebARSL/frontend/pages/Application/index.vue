a5880f4ea6bf267d3754bebc7e9f22f2219a188c
lordgravier <baskette11@gmail.com>
2020-01-17 10:34:02 +0100

    mise en place des button

---
+++ CODE/siteWebARSL/frontend/pages/Application/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <div class="text-center my-2">
+    <h1> Application </h1>
+    <hr>
+  </div>
+  <div class="mx-auto my-auto">
+    <b-row class="my-2" align-h="around">
+      <b-col cols="4"><b-button block variant="secondary">Formulaire</b-button></b-col>
+      <b-col cols="4"><b-button block variant="secondary">Maraudes</b-button></b-col>
+    </b-row>
+    <b-row class="my-2" align-h="around">
+      <b-col cols="4"><b-button block variant="secondary">Créer Groupes Maraudes</b-button></b-col>
+      <b-col cols="4"><b-button block variant="secondary">Alertes</b-button></b-col>
+    </b-row>
+    <b-row class="my-2" align-h="around">
+      <b-col cols="4"><b-button block variant="secondary">Trajet</b-button></b-col>
+      <b-col cols="4"><b-button block variant="secondary">Lieux</b-button></b-col>
+    </b-row>
+  </div>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+.appButton{
+  margin-left:20px;
+}
+h1,h2{
+font-family: OfficinaSansStd-Book, sans-serif;
+color: #f26340 ;
+}
+</style>
