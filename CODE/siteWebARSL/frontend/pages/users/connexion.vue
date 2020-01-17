9c22bed364f8a21c4cfdc312fff5e5892cee35e9
lordgravier <baskette11@gmail.com>
2020-01-17 14:23:24 +0100

    ajout personne.gql et personnes.gql

--- CODE/siteWebARSL/frontend/pages/users/connexion.vue
+++ CODE/siteWebARSL/frontend/pages/users/connexion.vue
@@ -3,49 +3,27 @@
 
   <div class="uk-child-width-1-2@m uk-grid">
       <div>
-          <div class="uk-card uk-card-default uk-card-small uk-card-body">
-            <img src="https://assets-ouch.icons8.com/preview/294/e25a0374-0657-45d5-98d9-2408473a744c.png" height="500" width="500" class="uk-align-center" alt="">
-          </div>
-      </div>
-      <div>
           <div class="uk-card uk-card-default uk-card-large uk-card-body">
 
               <form @submit.stop.prevent="handleSubmit">
                   <fieldset class="uk-fieldset">
 
-                      <legend class="uk-legend">Register</legend>
-
                       <div class="uk-margin">
-                        <label class="uk-form-label">Username</label>
-                        <input class="uk-input" v-model="username" type="text" placeholder="pbocuse">
+                        <label class="uk-form-label">Nom</label>
+                       <input class="uk-input" v-model="username" type="text" placeholder="nom">
                       </div>
 
                       <div class="uk-margin">
-                        <label class="uk-form-label" for="form-stacked-text">Email</label>
-                        <input class="uk-input" v-model="email" type="email" placeholder="paul.bocuse@gmail.com">
-                      </div>
-
-                      <div class="uk-margin">
-                        <label class="uk-form-label" for="form-stacked-text">Password</label>
+                        <label class="uk-form-label" for="form-stacked-text">Mot de passe</label>
                         <input class="uk-input" v-model="password" type="password">
                       </div>
 
                       <div class="uk-margin">
-                        <button class="uk-button uk-button-primary uk-width-1-1" :disabled="loading" type="submit">Submit</button>
-                      </div>
-
-                      <div class="uk-margin">
-                        <p>
-                          Already have an account?
-                          <router-link :to="{ name: 'users-signin'}">
-                            Login
-                          </router-link>
-                        </p>
+                        <button class="uk-button uk-button-primary uk-width-1-1" :disabled="loading" type="submit">Se connecter</button>
                       </div>
 
                   </fieldset>
               </form>
-
           </div>
       </div>
   </div>
@@ -62,22 +40,18 @@ export default {
     return {
       email: '',
       password: '',
-      username: '',
       loading: false
     }
   },
   methods: {
-    // Method that will register your users
     async handleSubmit() {
       try {
         this.loading = true
-        const response = await strapi.register(
-          this.username,
+        const response = await strapi.login(
           this.email,
           this.password
         )
         this.loading = false
-        // Call your setUser mutation from your auth.js store file
         this.setUser(response.user)
         this.$router.go(-1)
       } catch (err) {
@@ -85,7 +59,6 @@ export default {
         alert(err.message || 'An error occurred.')
       }
     },
-    // Define all your needed mutations, here: auth/setUser
     ...mapMutations({
       setUser: 'auth/setUser'
     })
