8e1e5d48c2305f54a34dd35d8852edc10ccd2c79
Frédéric CANAUD <frederic.canaud@etu.unilim.fr>
2020-02-15 16:39:15 +0100

    MAJ Formulaire de maraudes

--- CODE/siteWebARSL/frontend/pages/formulaireMaraude/index.vue
+++ CODE/siteWebARSL/frontend/pages/formulaireMaraude/index.vue
@@ -3,46 +3,37 @@
       <b-card>
          <h1 class="text-align-center mt-3 text-center"> Formulaire de maraude pour personne rencontrée </h1>
          <b-form id="formulaireMaraude" @submit="onSubmit" @reset="onReset" v-if="show">
-            <b-tabs card fill>
-               <b-tab title="Identité" active>
+               <b-tab active>
 
-                  <b-form-group id="input-group-1" label="Nom :" label-for="input-1">
+                  <b-form-group id="input-group-1" label="Pseudo :" label-for="input-1">
                      <b-form-input
                         id="input-1"
-                        v-model="form.nom"
                         type="input-1"
                         required
-                        placeholder="Nom de famille..."
-                        ></b-form-input>
-                  </b-form-group>
-
-                  <b-form-group id="input-group-2" label="Prénom :" label-for="input-2">
-                     <b-form-input
-                        id="input-2"
-                        v-model="form.prenom"
-                        placeholder="Prénom..."
-                        ></b-form-input>
-                  </b-form-group>
-
-                  <b-form-group id="input-group-3" label="Pseudo :" label-for="input-3">
-                     <b-form-input
-                        id="input-3"
                         v-model="form.pseudo"
                         placeholder="Pseudo..."
                         ></b-form-input>
                   </b-form-group>
 
-                  <b-form-group id="input-group-4" label="Nationalité :" label-for="input-4">
+                  <b-form-group id="input-group-2" label="Langue parlée :" label-for="input-2">
                      <b-form-input
-                        id="input-4"
-                        v-model="form.nationalite"
+                        id="input-2"
+                        v-model="form.langueParlee"
                         placeholder="Nationalité..."
                         ></b-form-input>
                   </b-form-group>
 
-                  <b-form-group id="input-group-5" label="Information Famille :" label-for="input-5">
+                  <b-form-group id="input-group-3" label="Lieu de rencontre :" label-for="input-3">
+                     <b-form-input
+                        id="input-3"
+                        v-model="form.lieu"
+                        placeholder="Lieu de rencontre..."
+                        ></b-form-input>
+                  </b-form-group>
+
+                  <b-form-group id="input-group-4" label="Information Famille :" label-for="input-4">
                      <b-form-textarea
-                        id="input-5"
+                        id="input-4"
                         v-model="form.infoFamille"
                         placeholder="Information famille..."
                         rows="4"
@@ -50,25 +41,9 @@
                         ></b-form-textarea>
                   </b-form-group>
 
-                  <b-form-group id="input-group-6" label="Lieu de rencontre :" label-for="input-6">
-                     <b-form-input
-                        id="input-6"
-                        v-model="form.lieu"
-                        placeholder="Lieu de rencontre..."
-                        ></b-form-input>
-                  </b-form-group>
-
-               </b-tab>
-               <b-tab title="Santé">
-
-                  <b-form-group id="input-group-7" label="Etat de grossesse :" label-for="input-7">
-                     <b-form-radio id="input-7" v-model="selected" name="positif" value="true">Positif</b-form-radio>
-                     <b-form-radio id="input-7" v-model="selected" name="negatif" value="false">Négatif</b-form-radio>
-                  </b-form-group>
-
-                  <b-form-group id="input-group-8" label="Problèmes de santé :" label-for="input-8">
+                  <b-form-group id="input-group-5" label="Problèmes de santé :" label-for="input-5">
                      <b-form-textarea
-                        id="input-8"
+                        id="input-5"
                         v-model="form.problemeDeSante"
                         placeholder="Problème de santé..."
                         rows="4"
@@ -76,41 +51,22 @@
                         ></b-form-textarea>
                   </b-form-group>
 
-                  <b-form-group id="input-group-9" label="Autres problèmes :" label-for="input-9">
+                  <b-form-group id="input-group-6" label="Situation d'hébergement :" label-for="input-6">
+                        <b-form-radio v-model="selected" name="some-radios" value="A">Option A</b-form-radio>
+                        <b-form-radio v-model="selected" name="some-radios" value="B">Option B</b-form-radio>
+                  </b-form-group>
+                  <b-form-group >
+
                      <b-form-textarea
-                        id="input-9"
+                        id="input-6"
                         v-model="form.autre"
-                        placeholder="Autres problèmes..."
+                        placeholder="Situation d'hébergement..."
                         rows="4"
                         max-rows="8"
                         ></b-form-textarea>
                   </b-form-group>
 
                </b-tab>
-               <b-tab title="Hébergement">
-
-                  <b-form-group id="input-group-10" v-model="form.hebergementActuel" label="Situation actuelle d'hébergement de la personne :" label-for="input-10">
-                     <b-form-radio v-model="selected" name="true" value="A">Positif</b-form-radio>
-                     <b-form-radio v-model="selected" name="false" value="B">Négatif</b-form-radio>
-                  </b-form-group>
-
-                  <b-form-group id="input-group-11" v-model="form.dejaConnu" label="La personne est-elle déjà connu de la maraude ?" label-for="input-11">
-                     <b-form-radio v-model="selected" name="oui" value="true">Oui</b-form-radio>
-                     <b-form-radio v-model="selected" name="non" value="false">Non</b-form-radio>
-                  </b-form-group>
-
-                  <b-form-group id="input-group-12" v-model="form.demandeHebergement" label="La personne a déjà exprimé une demande d'hébergement ?" label-for="input-12">
-                     <b-form-radio v-model="selected" name="oui" value="true">Oui</b-form-radio>
-                     <b-form-radio v-model="selected" name="non" value="false">Non</b-form-radio>
-                  </b-form-group>
-
-                  <b-form-group id="input-group-13" v-model="form.demandeAcceptee" label="Le 115 a t-il répondu positivement à la demande ?" label-for="input-13">
-                     <b-form-radio v-model="selected" name="oui" value="true">Oui</b-form-radio>
-                     <b-form-radio v-model="selected" name="non" value="false">Non</b-form-radio>
-                  </b-form-group>
-                  
-               </b-tab>
-            </b-tabs>
             <div id="valider" class="d-flex justify-content-center ">
                <b-button type="submit" variant="primary">Valider le formulaire</b-button>
             </div>
@@ -123,9 +79,10 @@
      data() {
        return {
          form: {
-           email: '',
-           name: '',
-           food: null,
+           nom: '',
+           prenom: '',
+           pseudo: '',
+           nationalite: '',
            checked: []
          },
          show: true
@@ -136,19 +93,6 @@
          evt.preventDefault()
          alert(JSON.stringify(this.form))
        },
-       onReset(evt) {
-         evt.preventDefault()
-         // Reset our form values
-         this.form.email = ''
-         this.form.name = ''
-         this.form.food = null
-         this.form.checked = []
-         // Trick to reset/clear native browser form validation state
-         this.show = false
-         this.$nextTick(() => {
-           this.show = true
-         })
-       }
      }
    }
 </script>
