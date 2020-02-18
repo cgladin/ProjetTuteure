2966cdbc4396ca58481f5ea28a937175d545a358
lordgravier <baskette11@gmail.com>
2020-02-18 16:46:29 +0100

    modif lien header

--- CODE/siteWebARSL/frontend/components/Header.vue
+++ CODE/siteWebARSL/frontend/components/Header.vue
@@ -1,51 +1,51 @@
 
 <template>
-   <client-only>
+  <client-only>
 
-      <nav id="navigation" class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-         <div class="container">
-            <a href="/">
-               <b-img src="logo.png" height="60" width="60" fluid alt="Responsive logo"></b-img>
-            </a>
+    <nav id="navigation" class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+      <div class="container">
+        <a href="/">
+          <b-img src="logo.png" height="60" width="60" fluid alt="Responsive logo"></b-img>
+        </a>
 
-            <!-- burger sur telephone -->
-            <div class="row tel">
-               <b-button v-b-toggle.collapse-1 variant="primary" class="tel bg-dark">
-                  <span class="navbar-toggler-icon "></span>
-               </b-button>
-               <b-collapse id="collapse-1" class="mt-2">
-                  <b-card class="bg-dark">
-                     <router-link tag="a" class="nav-link text-white" to="/associations" exact>Associations</router-link>
-                     <router-link tag="a" class="nav-link text-white" to="/users/connexion" exact>Se connecter</router-link>
-                  </b-card>
-               </b-collapse>
-            </div>
+        <!-- burger sur telephone -->
+        <div class="row tel">
+          <b-button v-b-toggle.collapse-1 variant="primary" class="tel bg-dark">
+            <span class="navbar-toggler-icon "></span>
+          </b-button>
+          <b-collapse id="collapse-1" class="mt-2">
+            <b-card class="bg-dark">
+              <router-link tag="a" class="nav-link text-white" to="/associations" exact>Associations</router-link>
+              <router-link tag="a" class="nav-link text-white" to="/users/connexion" exact>Se connecter</router-link>
+            </b-card>
+          </b-collapse>
+        </div>
 
-            <!-- bouton sur pc -->
-            <ul class="navbar-nav ml-auto ordi">
-               <li class="nav-item">
-                  <div class="translate" id="google_translate_element"></div>
-               </li>
-               <li class="nav-item align-middle">
-                  <router-link tag="a" class="nav-link text-white" to="/associations" exact>Associations</router-link>
-               </li>
-               <li class="nav-item align-middle">
-                  <a class="nav-link text-white" href="http://localhost:1337/admin/auth/login">Se connecter</a>
-               </li>
-            </ul>
+        <!-- bouton sur pc -->
+        <ul class="navbar-nav ml-auto ordi">
+          <li class="nav-item">
+            <div class="translate" id="google_translate_element"></div>
+          </li>
+          <li class="nav-item align-middle">
+            <router-link tag="a" class="nav-link text-white" to="/associations" exact>Associations</router-link>
+          </li>
+          <li class="nav-item align-middle">
+            <router-link tag="a" class="nav-link text-white" to="/users/connexion" exact>Se connecter</router-link>
+          </li>
+        </ul>
 
-         </div>
+      </div>
 
-         <!-- javascript du translate -->
-         <script type="text/javascript">// <![CDATA[
-            function googleTranslateElementInit() {
-            new google.translate.TranslateElement({pageLanguage: 'fr', includedLanguages:'en,ar,ru', layout: google.translate.TranslateElement.InlineLayout.SIMPLE}, 'google_translate_element');
-            }
-            // ]]>
-         </script>
-         <script src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit" type="text/javascript"></script>
+      <!-- javascript du translate -->
+      <script type="text/javascript">// <![CDATA[
+      function googleTranslateElementInit() {
+        new google.translate.TranslateElement({pageLanguage: 'fr', includedLanguages:'en,ar,ru', layout: google.translate.TranslateElement.InlineLayout.SIMPLE}, 'google_translate_element');
+      }
+      // ]]>
+      </script>
+      <script src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit" type="text/javascript"></script>
 
-      </nav>
+    </nav>
 
-   </client-only>
+  </client-only>
 </template>
